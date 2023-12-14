3c73903c71ce23c82e8fc1fb625f007a82b58ee7
Jai Desai <44884343+jairav@users.noreply.github.com>
2023-12-14 16:14:50 -0500

    First drop

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scExploreR_example
-scExploreR running the Triana object: https://pubmed.ncbi.nlm.nih.gov/34811546/
+scExploreR running on the Triana object: https://pubmed.ncbi.nlm.nih.gov/34811546/
+
---
+++ app.R
@@ -0,0 +1,14 @@
+# Run the application 
+#shinyApp(ui = ui, server = server)
+library(scExploreR)
+#cat("getwd: ", getwd(), "\n")
+#cat("ls: ", list.files(getwd()), "\n")
+#cat("ls: ", system.file("www/Auto_Dictionary.Rmd", package = "scExploreR"), "\n")
+
+scExploreR::run_scExploreR(
+  object_path = "triana_et_al_2021.rds", 
+  config_path = "triana_et_al_2021-config.yaml"
+)
+
+
+
